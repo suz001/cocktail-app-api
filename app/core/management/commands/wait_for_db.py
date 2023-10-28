2fefda78754f0024ff75e336750eade6f1419c54
suz001 <53109792+suz001@users.noreply.github.com>
2023-10-28 17:56:58 -0400

    Fix linting issues

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,9 +3,10 @@ Django command to wait for the database to be available
 """
 
 import time
-from psycopg2 import OperationalError as  Psycopg20pError
+from psycopg2 import OperationalError as Psycopg20pError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for databse"""
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,6 +7,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommendTest(SimpleTestCase):
     """Test commands"""
@@ -21,11 +22,15 @@ class CommendTest(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patch_sleep, patched_check):
         """Test waiting for database when getting Operation Error"""
-        """For the first 2 times we call mock method and raise psycopg2 error, then raise operational error 3 times."""
-        """When Postgre not started, it's not ready to accept any connection--->psycopg2 error"""
-        """When database is ready to accept connection, but it hasn't set up the databse--->operational error"""
+        """For the first 2 times we call mock method and raise psycopg2 error
+        then raise operational error 3 times."""
+        """When Postgre not started,
+        it's not ready to accept any connection--->psycopg2 error"""
+        """When database is ready to accept connection,
+        but it hasn't set up the databse--->operational error"""
         """2 and 3 are arbitray number, after 5 calls return true"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] *3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
